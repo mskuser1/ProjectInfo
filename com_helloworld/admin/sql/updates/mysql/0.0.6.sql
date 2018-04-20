8b26c596afecdde2628d9f3b9e78abc3589173b2
mskuser1 <mskuser1@mind-shaker.com>
2018-04-20 11:51:30 +0100

    Add files via upload

---
+++ com_helloworld/admin/sql/updates/mysql/0.0.6.sql
@@ -0,0 +1,39 @@
+/*DROP TABLE IF EXISTS `#__helloworld`;
+
+CREATE TABLE `#__helloworld` (
+	`id`       INT(11)     NOT NULL AUTO_INCREMENT,
+	`greeting` VARCHAR(25) NOT NULL,
+	`published` tinyint(4) NOT NULL DEFAULT '1',
+	PRIMARY KEY (`id`)
+)
+	ENGINE =MyISAM
+	AUTO_INCREMENT =0
+	DEFAULT CHARSET =utf8;
+
+INSERT INTO `#__helloworld` (`greeting`) VALUES
+('Hello World!'),
+('Good bye World!');*/
+
+DROP TABLE IF EXISTS `#__helloworld`;
+
+CREATE TABLE `#__helloworld` (
+	`id`       INT(11)     NOT NULL AUTO_INCREMENT,
+	`description` VARCHAR(255) NOT NULL,
+	`url` varchar(255) NOT NULL,
+	`ftpUser`  varchar(25)    NOT NULL ,
+	`ftpPassword`   VARCHAR(25) NOT NULL ,
+	`ip` 	int(9) NOT NULL,
+	`databaseName` varchar(50) NOT NULL,
+	`databaseUser` varchar(50) NOT NULL,
+	`databasePassword` varchar(255) NOT NULL,
+	`projectType` varchar(255) NOT NULL,
+	`phpVersion`int(5) NOT NULL,
+	`obs`varchar(255) NOT NULL,
+	`tags`varchar(255) NOT NULL,
+	`published` TINYINT(1) NOT NULL,
+
+	PRIMARY KEY (`id`)
+)
+	ENGINE =MyISAM
+	AUTO_INCREMENT =0
+	DEFAULT CHARSET =utf8;
