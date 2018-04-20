c413ffc453d93e3ad39bef35a9e76c19a8d49cf0
mskuser1 <mskuser1@mind-shaker.com>
2018-04-20 11:59:06 +0100

    Update 0.0.6.sql

--- com_helloworld/admin/sql/updates/mysql/0.0.6.sql
+++ com_helloworld/admin/sql/updates/mysql/0.0.6.sql
@@ -17,19 +17,19 @@ INSERT INTO `#__helloworld` (`greeting`) VALUES
 DROP TABLE IF EXISTS `#__helloworld`;
 
 CREATE TABLE `#__helloworld` (
-	`id`       INT(11)     NOT NULL AUTO_INCREMENT,
+		`id`       INT(11)     NOT NULL AUTO_INCREMENT,
 	`description` VARCHAR(255) NOT NULL,
 	`url` varchar(255) NOT NULL,
-	`ftpUser`  varchar(25)    NOT NULL ,
+	`ftpUser`  varchar(24)    NOT NULL ,
 	`ftpPassword`   VARCHAR(25) NOT NULL ,
-	`ip` 	int(9) NOT NULL,
-	`databaseName` varchar(50) NOT NULL,
-	`databaseUser` varchar(50) NOT NULL,
-	`databasePassword` varchar(255) NOT NULL,
-	`projectType` varchar(255) NOT NULL,
-	`phpVersion`int(5) NOT NULL,
-	`obs`varchar(255) NOT NULL,
-	`tags`varchar(255) NOT NULL,
+	`ip` 	char(15) NOT NULL,
+	`databaseName` varchar(24) NOT NULL,
+	`databaseUser` varchar(24) NOT NULL,
+	`databasePassword` varchar(25) NOT NULL,
+	`projectType` char(25) NOT NULL,
+	`phpVersion`varchar(10) NOT NULL,
+	`obs` text NOT NULL,
+	`tags` text NOT NULL,
 	`published` TINYINT(1) NOT NULL,
 
 	PRIMARY KEY (`id`)
