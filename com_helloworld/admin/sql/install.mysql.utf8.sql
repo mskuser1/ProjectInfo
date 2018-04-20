aa3248126847a9e04cf5197577547d6c8ca81968
mskuser1 <mskuser1@mind-shaker.com>
2018-04-20 11:56:37 +0100

    Update install.mysql.utf8.sql

--- com_helloworld/admin/sql/install.mysql.utf8.sql
+++ com_helloworld/admin/sql/install.mysql.utf8.sql
@@ -2,8 +2,20 @@ DROP TABLE IF EXISTS `#__helloworld`;
 
 CREATE TABLE `#__helloworld` (
 	`id`       INT(11)     NOT NULL AUTO_INCREMENT,
-	`greeting` VARCHAR(25) NOT NULL,
-	`published` tinyint(4) NOT NULL DEFAULT '1',
+	`description` VARCHAR(255) NOT NULL,
+	`url` varchar(255) NOT NULL,
+	`ftpUser`  varchar(25)    NOT NULL ,
+	`ftpPassword`   VARCHAR(25) NOT NULL ,
+	`ip` 	int(9) NOT NULL,
+	`databaseName` varchar(50) NOT NULL,
+	`databaseUser` varchar(50) NOT NULL,
+	`databasePassword` varchar(255) NOT NULL,
+	`projectType` varchar(255) NOT NULL,
+	`phpVersion`int(5) NOT NULL,
+	`obs`varchar(255) NOT NULL,
+	`tags`varchar(255) NOT NULL,
+	`published` TINYINT(1) NOT NULL,
+
 	PRIMARY KEY (`id`)
 )
 	ENGINE =MyISAM
